1e4bd1c70cf1565ffb892f0e5fe29607a735eb33
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-10-26 23:25:13 +1300

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,12 +1,6 @@
-<?php
-//database connection execute
-include_once('connect.php');
-?>
-
 <html>
 <head>
-
-  <link rel="stylesheet" type="text/css" href="Style.css">
+   <link rel="stylesheet" type="text/css" href="Style.css">
   
 <title>Registration</title>
 <body>
@@ -14,17 +8,21 @@ include_once('connect.php');
   <div class="form">
      <h1 class="register"> REGISTRATION </h1>
 
-     <form class="login-form" method="post">
-    
-       <input type="text" name="username" placeholder="username" required/>
-       
+     <form action="form_action_files/registration.php" class="login-form" method="post">
+       <input type="text" name="first_name" placeholder="First Name" required/>
+       <input type="text" name="last_name" placeholder="Last Name" required/>
+       <input type="text" name="username" placeholder="Username" required/>
+       <input type="text" name="mob_number" class="form-control" placeholder="Mobile number">
+       <label><input type="radio" name="user_type" class="radio-inline" value="customer" required> Customer</label>
+			 <label><input type="radio" name="user_type" class="radio-inline" value="trademan" required> Trademan</label>
+
        <input type="email" name="email" placeholder="E-mail" required/>
       
        <input type="password"  name="password" placeholder="password" required/>
      
        <button name="login" type="login">REGISTER</button>
       
-       <p class="message">Now you can Login!!<a href="Login.php">Login Page</a></p>
+       <p class="message">Now you can access Login!!<a href="Login.php">Login Page</a></p>
     
     </form>
   </div>
@@ -33,4 +31,3 @@ include_once('connect.php');
 </body>
 </head>
 </html>
-
