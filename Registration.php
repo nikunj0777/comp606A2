319fe8828416add954f92698b1002cd3f777bc6f
lubana-333 <52937205+lubana-333@users.noreply.github.com>
2019-11-01 22:52:29 +1300

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,10 +1,31 @@
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
    <link rel="stylesheet" type="text/css" href="Style.css">
   
 <title>Registration</title>
 	</head>
 <body>
+	
+<nav class="navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">SafeTrade</a>
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+        <li><a href="Login.php"><span class="glyphicon glyphicon-user"></span> login</a></li>
+				
+				</ul>
+			</div>
+    </nav> 
 <div class="login-page">
   <div class="form">
      <h1 class="register"> REGISTRATION </h1>
