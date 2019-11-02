d658238c95d9ac54687a34dc33be46149e1e3dbf
lubana-333 <52937205+lubana-333@users.noreply.github.com>
2019-11-03 00:01:49 +1300

    Update Login.php
    login file

--- Login.php
+++ Login.php
@@ -1,13 +1,15 @@
-<?php
-//database connection execute
-include_once('connect.php');
-?>
 <html>
 <head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
 <link rel="stylesheet" type="text/css" href="Style.css">
 <title>Login FORM</title>
 <body>
-  <nav class="navbar-default">
+<nav class="navbar-default">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<a class="navbar-brand" href="#">SafeTrade</a>
@@ -22,10 +24,12 @@ include_once('connect.php');
 			</div>
 		</nav> 
 
+
 <div class="login-page">
   <div class="form">
   <h1 class="login">LOGIN</h1>
-   <form action="form_action_files/login.php" class="login-form" method="post">
+    
+    <form action="form_action_files/login.php" class="login-form" method="post">
     
       <input type="text" name="username" placeholder ="username"/>
       <input type ="password"  name ="password" placeholder ="password"/>
@@ -34,7 +38,6 @@ include_once('connect.php');
       <p class="message">Not registered? <a href="Registration.php">Create an account</a></p>
     
     </form>
- 
   </div>
 </div>
 
