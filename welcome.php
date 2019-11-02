646bc2616935a9cda5ab329c05e675b66909bd99
lubana-333 <52937205+lubana-333@users.noreply.github.com>
2019-11-02 23:55:47 +1300

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -7,11 +7,12 @@ $user_id = $_SESSION['user_id'];
 $username = $_SESSION['username'];
 
 
-echo '<h1>Welcome to Trademe! '.$username.'</h1>';
+
+
 
 if(isset($user_type) && $user_type == 'customer')
 {
-  
+    echo '<h1>Welcome to customer! '.$username.'</h1>';
         
     echo '<h2 class="">Request you to create your job first.</h2>
     <a href="insert_job.php">Create job</a>
@@ -20,6 +21,7 @@ if(isset($user_type) && $user_type == 'customer')
 }
 else
 {
+    echo '<h1>Welcome to trademan! '.$username.'</h1>';
 
     echo '<h1 class="job_title">Are you findind job ?</h2><a href="trademan_job.php"> view of my bid job!</a>';
 
