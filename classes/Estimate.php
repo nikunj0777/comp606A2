e148d9b4e1eceefba9f5369c316bd84f07915ddf
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-11-04 18:57:12 +1300

    Update Estimate.php
    Updated File

--- classes/Estimate.php
+++ classes/Estimate.php
@@ -1,119 +1,132 @@
 <?php 
-class Estimate
-{
-    private $id = null;
-    private $job_id = "";
-    private $material_cost = "";
-    private $labor_cost = "";
-    private $total_cost = "";
-    private $starting_date = "";
-    private $expiring_date = "";
+/*
+    This is Job class which extends from the Database class.
+    So that it can always get connected with the database.
 
-    // constructor to create new estimate object
-    public function __construct($id, $job_id, $material_cost, $labor_cost, $total_cost, $starting_date, $expiring_date){
-        $this->id = $id;
-        $this->job_id = $job_id;
-        $this->material_cost = $material_cost;
-        $this->labor_cost = $labor_cost;
-        $this->total_cost = $total_cost;
-        $this->starting_date = $starting_date;
-        $this->expiring_date = $expiring_date;
+    some function in it:
+
+    1.create_job()
+    2.public_view_job()
+    3.customer_view_job()
+    4.customer_edit_job()
+    5.customer_delete_job()
+    6.trademan_view_job()
+    7.admin_view_job()
+    8.admin_delete_job()
+*/
+class Job
+{
+
+    private $user_id = null;
+    private $location = "";
+    private $job_type = "";
+    private $job_category = "";
+    private $payrate = "";
+    private $start_date = "";
+    private $end_date = "";
+
+    // constructor to create new student object
+    public function __construct($user_id, $location, $job_type, $job_category, $payrate, $start_date,$end_date){
+        $this->user_id = $user_id;
+        $this->location = $location;
+        $this->job_type = $job_type;
+        $this->job_category = $job_category;
+        $this->payrate = $payrate;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
     }
     // setter methods
-    public function setId($id){
+    public function setUser_id($user_id){
         $this->$id = $id;
       }
-    public function setJob_id($job_id){
-        $this->$job_id = $job_id;
-      }
-    public function setMaterial_cost($material_cost){
-    $this->$material_cost = $material_cost;
+    public function setLocation($location){
+    $this->$location = $location;
     }
-    public function setLabor_cost($labor_cost){
-        $this->$labor_cost = $labor_cost;
+    public function setJob_type($job_type){
+        $this->$job_type = $job_type;
     }
-    public function setTotal_cost($total_cost){
-    $this->$total_cost = $total_cost;
+    public function setJob_category($job_category){
+    $this->$job_category = $job_category;
     }
-    public function setStarting_date($starting_date){
-        $this->$starting_date = $starting_date;
+    public function setPayrate($payrate){
+        $this->$payrate = $payrate;
     }
-    public function setExpiring_date($expiring_date){
-    $this->$expiring_date = $expiring_date;
+    public function setStart_date($start_date){
+    $this->$start_date = $start_date;
     }
-
+    public function setEnd_date($end_date){
+        $this->$end_date = $end_date;
+    }
     //getter methods
-    public function getId(){
-        return $this->id;
-    }
-    public function getJob_id(){
-        return $this->job_id;
-    }
-    public function getMaterial_cost(){
-        return $this->material_cost;
-    }
-    public function getLabor_cost(){
-        return $this->labor_cost;
-    }
-    public function getTotal_cost(){
-        return $this->total_cost;
-    }
-    public function getStarting_date(){
-        return $this->starting_date;
-    }
-    public function getExpiring_date(){
-        return $this->expiring_date;
-    }
 
-    public static function create_estimate($mysqli, $job_id, $trademan_id, $material_cost, $labor_cost, $total_cost, $starting_date, $expiring_date) 
+    public function getUser_id(){
+        return $this->user_id;
+    }
+    public function getLocation(){
+        return $this->location;
+    }
+    public function getJob_type(){
+        return $this->job_type;
+    }
+    public function getJob_category(){
+        return $this->job_category;
+    }
+    public function getPayrate(){
+        return $this->payrate;
+    }
+    public function getStart_date(){
+        return $this->start_date;
+    }
+    public function getEnd_date(){
+        return $this->end_date;
+    }
+    //Customer can post job 
+    public static function create_job($mysqli, $user_id, $location, $job_type, $job_category, $payrate, $start_date,$end_date) 
     {
-        $sql2 = "SELECT count(*) FROM `estimate` WHERE `job_id` =$job_id";
-        $result2 = $mysqli->query($sql2) or die($mysqli->error);
-        $estimate_count = $result2->fetch_array(MYSQLI_ASSOC);
-       
-        if($result2)
-        {
-            $sql1 = "UPDATE jobs SET `job_status`= 'Yes', `trademan_id`= $trademan_id WHERE `id`= $job_id";
-            $result1 = $mysqli->query($sql1) or die($mysqli->error);
-           
-        }
+      
+        $user_id = $_SESSION['id'];
+        $sql = "INSERT INTO jobs VALUES ('0', '$user_id', '$location', '$job_type', '$job_category','$payrate', '$start_date', '$end_date','No','0')";
+        $result = $mysqli->query($sql) or die($mysqli->error);
 
-        $sql = "INSERT INTO estimate VALUES ('0', '$job_id', '$trademan_id', '$material_cost', '$labor_cost', '$total_cost', '$starting_date', '$expiring_date', 'Pending')";
-        $result1 = $mysqli->query($sql) or die($mysqli->error);
-        
+        if($result){
+			$job_id = $mysqli->insert_id;
+			$job = new Job($job_id, $user_id, $location, $job_type, $job_category, $payrate, $start_date,$end_date);
+		    $result = $job;
+		}
+			return $result;
     }
 
-    public static function trademan_view_estimate($mysqli, $uid)
+   
+    //This function is for everyone can view all the job on home page no need to login
+    public static function public_view_job($mysqli) 
     {
-        $id = $_SESSION['id'];
-        $username = $_SESSION['username'];
-
-        $sql = "SELECT * FROM estimate WHERE `trademan_id`= $uid";
-        $result =  $mysqli->query($sql) or die($mysqli->error);
+        $sql = "SELECT * FROM job";
+        $result = $mysqli->query($sql) or die($mysqli->error);
+        $public_jobview = $result->fetch_array(MYSQLI_ASSOC);
+        /*$result = $this->db->prepare($sql);
+        $result->execute();*/
       
         ?>
+        <div>
         <table class = "table table-hover table-sm table-light table-striped">
         <thead class="table-success">
         <tr>
         <th>Job ID</th>
-        <th>trademan ID(Your ID)</th>
-        <th>Material Cost</th>
-        <th>Labor Cost</th>
-        <th>Total Cost</th>
+        <th>Creater Customer ID</th>
+        <th>Location</th>
+        <th>Description</th>
+        <th>Estimated Total Cost</th>
         <th>Job Start Date</th>
         <th>Job Expire Date</th>
-        <th>Confirmation</th>
-        <th></th>
-
-        <!-- <th>Contact</th> -->
+        <th>Any Trademan Interested?</th>
+      
 
         </tr>
         </thead>
         <tbody>
         <?php
      
-        foreach ($result as $key => $res) 
-        {
+     foreach ($result as $key => $res){
             
             ?>
             
@@ -121,107 +134,192 @@ class Estimate
                 
                 <tr>
                 <td><?= $res['job_id']; ?> </td>
-                <td><?= $res['trademan_id']; ?> </td>
-                <td><?= $res['material_cost']; ?> </td>
-                <td><?= $res['labor_cost']; ?> </td>
-                <td><?= $res['total_cost']; ?> </td>
-                <td><?= $res['starting_date']; ?> </td>
-                <td><?= $res['expiring_date']; ?> </td> 
-                <td><?= $res['confirm'];?> </td>
-                <td><a href="form_action_files/estimate_delete.php?id=<?php echo $res["id"];?>&job_id=<?php echo $res["job_id"];?>" onClick="return confirm('Are you sure?')">Delete</a></td>
+                <td><?= $res['user_id']; ?> </td>
+                <td><?= $res['job_location']; ?> </td>
+                <td><?= $res['job_description']; ?> </td>
+                <td><?= $res['job_price']; ?> </td>
+                <td><?= $res['job_start_date']; ?> </td>
+                <td><?= $res['job_expire_date']; ?> </td> 
+                <td><?= $res['job_status']; ?> </td>
+       
+
                 </tr>
             </div>
 
         <?php
-        
         }?>
         </tbody>
         </table>
         <?php
-        return $result;
     }
 
-    public static function trademan_delete_bid($mysqli, $estimate_id, $job_id)
+    //Function to view customer jobs
+    public static function customer_view_job($mysqli, $user_id) 
     {
-        //delete the bid 
-        $sql = "DELETE FROM `estimate` WHERE `estimate`.`id` = $estimate_id";
+        // $user_id = $_SESSION['uid'];
+
+        $sql = "SELECT * FROM jobs WHERE (user_id = '$user_id')";
+        /*$result = $this->db->prepare($sql);
+        $result->execute();*/
         $result = $mysqli->query($sql) or die($mysqli->error);
-
-        $sql2 = "SELECT count(*) FROM `estimate` WHERE `job_id` =$job_id";
-        $result2 = $mysqli->query($sql2) or die($mysqli->error);
-        
-        $rows = $result2->fetch_array(MYSQLI_ASSOC);
-        //print_r($rows);
-        $row = $rows[0];
-
-        if($row == 0)
-        {
-            $sql1 = "UPDATE jobs SET `job_status`= 'No', `trademan_id`= 0 WHERE `id`= $job_id";
-            $result1 = $mysqli->query($sql1) or die($mysqli->error);
-           
-                }
-    }
-
-    public static function customer_view_estimate($mysqli, $job_id)
-    {
-        $id = $_SESSION['id'];
-        $username = $_SESSION['username'];
-    
-        $sql = "SELECT * FROM estimate WHERE `job_id`= $job_id";
-        $result = $mysqli->query($sql) or die($mysqli->error);
+        $customer_jobresult = $result->fetch_array(MYSQLI_ASSOC);
         
         ?>
-        <table class = "table table-hover table-sm table-light table-striped">
-        <thead class="table-success">
+        
+        <table class = "table ">
+        <thead class = "table-success">
         <tr>
         <th>Job ID</th>
-        <th>trademan ID</th>
-        <th>Material Cost</th>
-        <th>Labor Cost</th>
-        <th>Total Cost</th>
+        <th>Location</th>
+        <th>Job Type</th>
+        <th>job category</th>
+        <th>job payrate</th>
         <th>Job Start Date</th>
-        <th>Job End Date</th>
-        <th>Confirmation</th>
-
+        <th>Job Expire Date</th>
+        <th>Any Bid</th>
+        <!-- <th>Trademan's ID(if someone bid)</th> -->
+        <th></th>
         </tr>
         </thead>
         <tbody>
         <?php
 
-        $sql2 = "SELECT count(*) FROM `estimate` WHERE `job_id` =$job_id";
-        $result2 = $mysqli->query($sql2) or die($mysqli->error);
-        $rows = $result2->fetch_array(MYSQLI_ASSOC);
-        //$row = $rows[0];
-        
-        foreach ($result as $key => $res){     ?>
-            
-            <div class="container-fluid">     
+       
+        //print_r($customer_jobresult);
+        //die("m here");
+        foreach ($result as $key => $res) 
+        {
+            ?>
+            <div class="container-fluid">
+                
                 <tr>
-                <td><?= $res['job_id']; ?> </td>
-                <td><a href="trademan_contact.php?trademan_id=<?php echo $res['trademan_id']?>"><?= $res['trademan_id']; ?></a></td>
-                <td><?= $res['material_cost']; ?> </td>
-                <td><?= $res['labor_cost']; ?> </td>
-                <td><?= $res['total_cost']; ?> </td>
-                <td><?= $res['starting_date']; ?> </td>
-                <td><?= $res['expiring_date']; ?> </td> 
-                <td><?= $res['confirm'];?></td>
+                <td><?= $res['id']; ?> </td>
+                <td><?= $res['location']; ?> </td>
+                <td><?= $res['job_type']; ?> </td>
+                <td><?= $res['job_category']; ?> </td>
+                <td><?= $res['payrate']; ?> </td>
+                <td><?= $res['start_date']; ?> </td> 
+                <td><?= $res['end_date']; ?> </td> 
+                <td><a href="view_bid.php?job_id=<?php echo $res['id']?>&job_category=<?php echo $res['job_category']?>&job_status=<?php echo $res['job_status']?>"><?= $res['job_status']; ?> </td>
+                <td><a href="customer_edit_job.php?job_id=<?php echo $res['id']?>&user_id=<?php echo $res['user_id']?>&location=<?php echo $res['location']?>&job_category=<?php echo $res['job_category']?>&job_type=<?php echo $res['job_type']?>&payrate=<?php echo $res['payrate']?>&start_date=<?php echo $res['start_date']?>&end_date=<?php echo $res['end_date']?>">Edit</a> 
+                |<a href="form_action_files/customer_delete.php?job_id=<?php echo $res['id']?>" onClick="return confirm('Are you sure?')">Delete</a></td>	
+
                 </tr>
             </div>
-                
-            <?php } ?>
+
+        <?php
+
+                   
+        }?>
+        </tbody>
+        </table>
+        
+        <?php
+            if(isset($res))
+            {
+                // $_SESSION['job_id'] = $res['job_id'];
+                $_SESSION['trademan_id'] = $res['trademan_id'];
+            
+            }
+    }
+
+    //This function is for login customer to edit his/her posted jobs
+    public static function customer_edit_job($mysqli, $job_id, $location, $job_type, $job_category, $payrate, $start_date, $end_date) 
+    {
+
+
+        $sql = "UPDATE jobs SET location = '$location', job_type = '$job_type', job_category = '$job_category', payrate = '$payrate', start_date = '$start_date', end_date = '$end_date' WHERE id = '$job_id'";
+        $result = $mysqli->query($sql) or die($mysqli->error);
+        if($result){
+            return $result;
+        }
+        
+    }
+
+    //This function is for delete  posted jobs
+    public static function customer_delete_job($mysqli, $job_id) 
+    {
+        // session_start();
+        $sql = "DELETE FROM jobs WHERE id = $job_id";
+        $result = $mysqli->query($sql) or die($mysqli->error);
+       
+        
+
+    }
+
+    //This function is for login trademan to view all posted jobs 
+    //Plus he/she can bid the job!
+    public static function trademan_view_job($mysqli, $user_id) 
+    {
+       
+
+        $sql = "SELECT * FROM jobs";
+        $result = $mysqli->query($sql) or die($mysqli->error);
+        $trademan_jobview = $result->fetch_array(MYSQLI_ASSOC);
+        
+        ?>
+        <table class = "table table-hover table-sm table-light table-striped">
+        <thead class = "table-success">
+        <tr>
+        <th>Job ID</th>
+        <th>Customer ID</th>
+        <th>Location</th>
+        <th>Job Type</th>
+        <th>Job category</th>
+        <th>Job Payrate</th>
+        <th>Job Start Date</th>
+        <th>Job Expire Date</th>
+        <th>Any Trademan Interested?</th>
+        <th>Give a Bid</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+
+        foreach ($result as $key => $res) 
+        {    
+            ?> 
+            <div class="container-fluid">
+                <tr>
+                <td><?= $res['id']; ?> </td>
+                <td><?= $res['user_id']; ?> </td>
+                <td><?= $res['location']; ?> </td>
+                <td><?= $res['job_type']; ?> </td>
+                <td><?= $res['job_category']; ?> </td>
+                <td><?= $res['payrate']; ?> </td>
+                <td><?= $res['start_date']; ?> </td>
+                <td><?= $res['end_date']; ?> </td> 
+                <td><?= $res['job_status']; ?> </td>
+
+          <?php
+            //first check if the trademan bid or not
+            $sql1 = "SELECT * FROM estimate WHERE trademan_id= $user_id";
+            $result1 = $mysqli->query($sql) or die($mysqli->error);
+            $res1 = $result1->fetch_array(MYSQLI_ASSOC);
+            
+            // button disabled
+            if($res1['trademan_id'] == $user_id && $res['job_status'] == 'Got bid' && $res1['job_id']== $res['job_id'] )
+            {?>
+                <td><a href="bid.php?job_id=<?php echo $res['id']?>&job_status=<?php echo $res['job_status']?>"><button disabled class="btn btn-outline-primary">Already Bid</button></td></a>
+            <?php
+            }else{ // button OK
+            ?>
+                <td><a href="bid.php?job_id=<?php echo $res['id']?>&job_status=<?php echo $res['job_status']?>"><button class="btn btn-outline-primary">Bid</button></td></a>
+              
+                </tr>
+            </div>
+
+            <?php
+            }
+        }?>
         </tbody>
         </table>
         <?php
-   }
-
-    public static function customer_confrim($mysqli, $job_id, $trademan_id)
-     {
-        $sql = "UPDATE estimate SET `confirm`= 'Confirmed' WHERE `job_id`= $job_id AND `trademan_id` = $trademan_id";
-        $result = $mysqli->query($sql) or die($mysqli->error);
-        return $result;
-
-        $sql1 = "UPDATE estimate SET `confirm`= 'Refused' WHERE `confirm`= 'Pending'";
-        $result1 = $mysqli->query($sql1) or die($mysqli->error);
-        return $result1;
+                if(isset($res))
+                {
+                    // $_SESSION['job_id'] = $res['job_id'];
+                    $_SESSION['user_id'] = $res['user_id'];
+                
+                }
     }
 }
